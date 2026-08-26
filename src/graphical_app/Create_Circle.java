c83f4f6480281f0ca245367c40be5f14b1aae9f3
Ali-k-pz <zahedpanahali@gmail.com>
2026-08-26 13:47:27 +0330

    Starting the Project

---
+++ src/graphical_app/Create_Circle.java
@@ -0,0 +1,13 @@
+package graphical_app;
+
+public class Create_Circle {
+    
+    
+    public Create_Circle(){
+        
+    }
+    
+    private int Radius,Center_Point_X,Center_Point_Y;
+    private float X_Point, Y_Point;
+
+}
---
+++ src/graphical_app/Graphical_App.java
@@ -0,0 +1,11 @@
+
+package graphical_app;
+
+
+public class Graphical_App {
+
+    public static void main(String[] args) {
+        Create_Circle C = new Create_Circle();
+    }
+    
+}
